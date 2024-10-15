c591a514c5955a6a978ab0204d8ad9bd6664d740
Jorge <u201921734@upc.edu.pe>
2024-10-15 17:38:09 -0500

    chore: created base project

---
+++ README.md
@@ -0,0 +1,3 @@
+# trackmyroute_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/trackmyroute/trackmyroute_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.trackmyroute.trackmyroute_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
