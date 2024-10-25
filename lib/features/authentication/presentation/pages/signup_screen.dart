e2e894f673a3b9cba533baaa79de4e8620176429
Jorge <u201921734@upc.edu.pe>
2024-10-24 19:06:41 -0500

    feat: implemented new version of auth service

--- lib/features/authentication/presentation/pages/signup_screen.dart
+++ lib/features/authentication/presentation/pages/signup_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trackmyroute_flutter/features/authentication/presentation/blocs/auth_bloc.dart';
 import 'package:trackmyroute_flutter/features/authentication/presentation/blocs/auth_event.dart';
+import 'package:trackmyroute_flutter/features/authentication/presentation/blocs/auth_state.dart';
 import 'package:trackmyroute_flutter/shared/presentation/home_page.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -17,40 +18,49 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        margin: const EdgeInsets.all(20.0),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                controller: _userController,
-                decoration: const InputDecoration(labelText: 'Usuario'),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _pwController,
-                decoration: const InputDecoration(labelText: 'Contraseña'),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) =>  const HomePage(),
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (BuildContext context, AuthState state) {
+        if (state is AuthInitialState){
+          return Center(
+            child: Card(
+              margin: const EdgeInsets.all(20.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      controller: _userController,
+                      decoration: const InputDecoration(labelText: 'Usuario'),
                     ),
-                  );
-                  context.read<AuthBloc>().add(RegisterUser(user: _userController.text, password: _pwController.text));
-                },
-                child: const Text('Registrarse'),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _pwController,
+                      decoration: const InputDecoration(labelText: 'Contraseña'),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(RegisterUser(user: _userController.text, password: _pwController.text));
+                      },
+                      child: const Text('Registrarse'),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        } else if (state is AuthLoadingState) {
+          return const Center (
+            child: SizedBox (
+              width: 30, height: 30,
+              child: CircularProgressIndicator())); 
+        } else {
+          return const HomePage();
+        }
+      },
     );
+    
   }
 }
--- lib/shared/presentation/home_page.dart
+++ lib/shared/presentation/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trackmyroute_flutter/features/profile/presentation/profile_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,17 +9,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>{
+  int _selectedIndex = 0; // Para controlar la pestaña seleccionada
+
+  // Lista de widgets para las diferentes vistas
+  final List<Widget> _views = [
+    const SearchRoutesScreen(), 
+    const ProfileScreen(),        // Vista para el perfil
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Track My Route'),
+        backgroundColor: Colors.teal,
+      ),
+      body: _views[_selectedIndex], // Muestra la vista seleccionada
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, // Index de la pestaña seleccionada
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index; // Cambia la pestaña seleccionada
+          });
+        },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar Rutas'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: 'Mis Favoritos'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'), // Esta pestaña muestra el perfil
         ],
       ),
+    );
+  }
+}
+
+class SearchRoutesScreen extends StatelessWidget {
+  const SearchRoutesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _searchController = TextEditingController();
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Buscar rutas...',
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: const Icon(Icons.search, color: Colors.grey),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Text(
+              'Aquí puedes buscar rutas',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
