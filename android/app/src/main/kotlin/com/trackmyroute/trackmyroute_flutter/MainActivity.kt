610f36d755d06d9ac53fb4260d273c6e990b5fb0
Jorge <u201921734@upc.edu.pe>
2024-11-21 17:38:54 -0500

    chore: modified android files to create release apk

--- android/app/src/main/kotlin/com/trackmyroute/trackmyroute_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/trackmyroute/trackmyroute_flutter/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.trackmyroute.trackmyroute_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
